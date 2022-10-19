d04cefa71adf500ad7c142d922d519790ea212c8
Abu Siam <siamcse01@gmail.com>
2022-10-20 00:23:29 +0600

    component added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdt_V366Eya8z195YAFEDvc9M5Y5-MEnQ",
+  authDomain: "lion-news-c1b01.firebaseapp.com",
+  projectId: "lion-news-c1b01",
+  storageBucket: "lion-news-c1b01.appspot.com",
+  messagingSenderId: "197234829667",
+  appId: "1:197234829667:web:9e6e067ca62e3644bccb56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaWhatsapp, FaTwitter, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle/> Login with Google</Button>
+                <Button className='mb-2' variant="outline-dark"><FaGithub/> Login with Github</Button>
+            </ButtonGroup>
+            <div>
+                <h4>Find Us on:</h4>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel/>
+            </div>
+        </div>
+    );
+};
+
+export default RightSideNav;
